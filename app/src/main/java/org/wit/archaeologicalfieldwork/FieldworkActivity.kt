daa164ac639a70f40e81ab50bfb4b5de48366265
Dylan-Morrissey <dylanmorrissey2010@gmail.com>
2019-10-16 11:00:13 +0100

    Initial Build

---
+++ app/src/main/java/org/wit/archaeologicalfieldwork/FieldworkActivity.kt
@@ -0,0 +1,12 @@
+package org.wit.archaeologicalfieldwork
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FieldworkActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fieldword)
+    }
+}

b3ab374206d499b3e6624e4d89fcdd4f1d85c6ab
Dylan-Morrissey <dylanmorrissey2010@gmail.com>
2019-10-17 17:36:45 +0100

    Worked on Login Page have a gradient Background and an email and passowrd field.

---
+++ app/src/main/java/org/wit/archaeologicalfieldwork/LoginActivity.kt
@@ -0,0 +1,15 @@
+package org.wit.archaeologicalfieldwork
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+
+class LoginActivity : AppCompatActivity(), AnkoLogger {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fieldword)
+        info("Login Activity Started...")
+    }
+}
